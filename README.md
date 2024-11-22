86970ec50ce8d47ea75e8a6421af7cdf528fe78a
Chakravarthy <115223455+chakysparkle@users.noreply.github.com>
2024-11-22 18:03:19 +0530

    adding files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Webgi-nextjs-variation-configurator
+# Custom UI for VariationConfiguratorPlugin
+
+This repository contains a code demo showcasing how to create an advanced custom user interface (UI) using React, Nextjs, TypeScript, and the VariationConfiguratorPlugin.
+
+## Getting Started
+
+To get started with the code demo, follow these steps:
+
+1. Clone the repository to your local machine.
+2. Install the required dependencies by running `npm install`.
+3. Start the development server by running `npm run dev`.
+
+## Learn More
+
+For more information and in-depth tutorials on how to use the VariationConfiguratorPlugin, please refer to https://webgi.xyz/docs/manual/variationConfigurator
---
+++ components/WebgiApp.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { ViewerApp, addBasePlugins, AssetManagerPlugin, AssetImporter, VariationConfiguratorPlugin, EXRLoadPlugin, DiamondPlugin } from "webgi";
+
+export default function WebgiApp(props: any) {
+  const [viewer, setViewer] = useState<ViewerApp>();
+  useEffect(() => {
+    setupViewer();
+    return () => {
+      disposeViewer();
+    };
+  }, []);
+
+  async function setupViewer() {
+    // Initialize the viewer
+    let viewer = new ViewerApp({
+      canvas: document.getElementById("webgi-canvas") as HTMLCanvasElement,
+      useRgbm: true,
+      useGBufferDepth: true,
+      isAntialiased: false,
+    });
+
+    viewer.renderer.displayCanvasScaling = Math.min(window.devicePixelRatio, 1);
+
+    await addBasePlugins(viewer);
+
+    /*await viewer.addPlugin(EXRLoadPlugin);*/
+
+    await viewer.addPlugin(DiamondPlugin);
+
+    const manager = await viewer.getPlugin(AssetManagerPlugin)!;
+    await viewer.renderer.refreshPipeline();
+
+    const config = await viewer.addPlugin(VariationConfiguratorPlugin);
+    await config.importPath("config.json");
+    manager.addFromPath("https://3dassetsmaan.s3.ap-south-1.amazonaws.com/VariationConfiguratorTutorials/1/scene.vjson");
+
+    setViewer(viewer);
+    props.setConfig(config);
+  }
+
+  function disposeViewer() {
+    if (!viewer) return;
+    viewer.scene.disposeSceneModels();
+    viewer.scene.dispose();
+    viewer.renderer.dispose();
+    viewer.dispose();
+  }
+
+  return (
+    <div id="webgi-canvas-container" className="w-full h-full">
+      
+      <canvas id="webgi-canvas" className="w-full h-full"></canvas>
+    </div>
+  );
+}
